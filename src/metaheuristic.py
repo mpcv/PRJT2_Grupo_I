0381aa5ff9c1331bf72b85f87d6646931b6d230f
Seu Nome <1220827@isep.ipp.pt>
2026-07-13 21:08:40 +0100

    Fase 2 - continuação

--- src/metaheuristic.py
+++ src/metaheuristic.py
@@ -80,7 +80,7 @@ class SimulatedAnnealing:
         new_sol.start_times = {}
         return new_sol
     
-    def _neighbor_double_mode_changes (self,solution):
+    def _neighbor_double_mode_change (self,solution):
     #Troca o modo de duas tarefas em simultâneo para tentar contornar a falta de recursos N
         candidatos = [job for job in self.instance.modes_data.keys()
                         if len(self.instance.modes_data[job]) > 1]
@@ -100,3 +100,106 @@ class SimulatedAnnealing:
 
         if not outros_modos1 or not outros_modos2:
             return None
+        
+        novo_modo1 = random.choice(outros_modos1)
+        novo_modo2 = random.choice(outros_modos2)
+
+        new_sol = self._clone_solution(solution)
+        new_sol.modes[job1] = novo_modo1
+        new_sol.modes[job2] = novo_modo2
+        new_sol.start_times = {}
+        return new_sol
+    
+    def _generate_neighbor(self, solution):
+        r= random.random()
+        #40% de probabilidade de trocar a ordem de execução
+        if r < 0.40:
+            neighbor = self._neighbor_swap_sequence(solution)
+        #30% de probabilidade de mudar o modo de UMA tarefa
+        elif r < 0.70:
+            neighbor = self._neighbor_change_mode(solution)
+        #30% de probabilidade de mudar o modo de DUAS tarefas (Estratégia de Desbloqueio)
+        else:
+            neighbor = self._neighbor_double_mode_change(solution)
+
+        #Fallback de segurança caso a vizinhança escolhida devolva None
+        if neighbor is None:
+            neighbor = self._neighbor_change_mode(solution) if random.random() < 0.5 \
+                else self._neighbor_swap_sequence(solution)
+        
+        return neighbor
+    
+    def run (self, initial_solution, time_limit=None, max_iterations=None, target_makespan=None):
+        start_time = time.time()
+
+        current = self._clone_solution(initial_solution)
+        if not current.is_feasible or current.makespan == 0:
+            self.builder.evaluate(current)
+
+        best = self._clone_solution(current)
+        history = [best.makespan]
+        total_iterations = 0
+
+        restarts = 0
+        max_restarts_no_time_limit = 5
+
+        while True:
+            #Reaquecimento térmico controlado
+            if restarts == 0:
+                temperature = self.initial_temp
+            else:
+                #Voltar à melhor solução encontrada antes de dar o choque térmico
+                current = self._clone_solution(best)
+                temperature = self.initial_temp * 1.2
+
+            while temperature > self.min_temp:
+                for _ in range(self.iterations_per_temp):
+                    total_iterations += 1
+
+                    #Early Stopping: Aborta se atingir o alvo
+                    if target_makespan is not None and best.is_feasible and best.makespan <= target_makespan:
+                        print(f"    -> [SA] Ótimo/BKS ({target_makespan}) alcançado! A abortar pesquisa.")
+                        return best, history
+                    
+                    #Critério de paragem por tempo
+                    if time_limit is not None and (time.time() - start_time) >= time_limit:
+                        return best, history
+                    
+                    if max_iterations is not None and total_iterations >= max_iterations:
+                        return best, history
+                    
+                    neighbor = self._generate_neighbor(current)
+                    if neighbor is None:
+                        continue
+
+                    #Avaliação usando SSGS
+                    self.builder.evaluate(neighbor)
+
+                    #Penalização pesada se a solução não for viável
+                    if not neighbor.is_feasible:
+                        delta = 99999
+                    else:
+                        delta = neighbor.makespan - current.makespan
+                    
+                    #Aceitação
+                    if delta < 0 and neighbor.is_feasible:
+                        current = neighbor
+                        if current.makespan < best.makespan:
+                            best = self._clone_solution(current)
+                    else:
+                        probability = math.exp(-delta / temperature) if temperature > 0 else 0
+                        if random.random() < probability:
+                            current = neighbor
+                    
+                    history.append(best.makespan)
+
+                temperature *= self.cooling_rate
+            
+            restarts += 1
+            if time_limit is None and restarts >= max_restarts_no_time_limit:
+                break
+        
+        return best, history
+        
+
+
